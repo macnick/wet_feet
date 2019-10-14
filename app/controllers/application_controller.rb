79268b50d55f0233dda507206b11c0a065811fbd
addod19 <addodaniellarbi@gmail.com>
2019-10-14 19:09:26 +0000

    output ¡Hola, mundo!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, Daniel and Nick!"
+    render html: "¡Hola, mundo!"
   end
 end
